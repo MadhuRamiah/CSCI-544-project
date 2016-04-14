e1fec02d2d0e2aac6e3d2b23caa66f765185204a
MadhuRamiah <madhuramiah@gmail.com>
2016-04-14 13:58:17 -0700

    Added files via upload

--- parseData.py
+++ parseData.py
@@ -1,7 +1,7 @@
 # encoding: utf-8
 __author__ = 'Murugappan'
 
-import json
+import re
 import codecs
 
 data=[]
@@ -10,10 +10,10 @@ language_list=[]
 language_full={}
 id={}
 cluster_name={}
-with codecs.open("uniformly_sampled.json","r","utf-8") as f:
+with codecs.open("precision.json","r","utf-8") as f:
     lines = f.readlines() # read only the first tweet/line
 
-with codecs.open("uniformly_sampled.tsv","r","utf-8") as f:
+with codecs.open("precision_oriented.tsv","r","utf-8") as f:
     tsv=f.readlines()
 
 with open("cluster.txt","r") as f:
@@ -23,6 +23,7 @@ language_id=[]
 language_value=[]
 cluster_id=[]
 for cluster in clusters:
+    print(cluster)
     str=cluster.replace("\n","")
     str=str.split("   ")
     cluster_name[str[1]]=str[0]
@@ -51,12 +52,27 @@ for line in lines:
     i=i+1
 
 items=[]
-with codecs.open("train_uniformly_sampled.txt","w","utf-8") as f:
+with codecs.open("train_precision_oriented.txt","w","utf-8") as f:
+    f_test=codecs.open("test_precision_oriented.txt","w","utf-8")
     for item in language_dict:
         value=language_dict[item]
-        for val in value:
+        length=len(value)
+        train=int(0.8*length)
+        if train==0 and length>0:
+            train=1
+        print(train)
+        print(length-train)
+        print(length)
+        print("-----------------------------------------------")
+        test=length-train
+        for i in range(0,train):
             items.append(item)
-            f.write(cluster_name[item]+"\t"+item+"\t"+val+"\n")
+            text = re.sub(r"http\S+", "", value[i])
+            f.write(cluster_name[item]+"\t"+item+"\t"+text+"\n")
+        for i in range(train,length):
+            items.append(item)
+            text = re.sub(r"http\S+", "", value[i])
+            f_test.write(cluster_name[item]+"\t"+item+"\t"+text+"\n")
 f.close()
 
 #print(list(set(items)-set(cluster_id)))
