7dc48cc223ad76c982f44398dd9a8ba9db4dcca1
MadhuRamiah <madhuramiah@gmail.com>
2016-04-14 23:25:49 -0700

    Added files via upload

--- parseData.py
+++ parseData.py
@@ -4,31 +4,52 @@ __author__ = 'Murugappan'
 import re
 import codecs
 
+#regex for preprocessing
+regex_str = [
+    r'(?:[:=;][oO\-]?[D\)\]\(\]/\\OpP])',
+    r'<[^>]+>', # HTML tags
+    r'@[.]*[/]*[ ]*[,]*', # @-mentions
+    r'@[\w_]+', #@ removal
+    r"http\S+", # http removal
+    r"#[0-9]*",  #hash tag and number
+    r"(?:\#+[0-9]*[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
+    ]
+
+#function to remove unnecessary tags and urls
+def regex(data):
+    for reg in regex_str:
+        data = re.sub(reg, "", data)
+    return data
+
+
 data=[]
 language_dict=dict()
 language_list=[]
 language_full={}
 id={}
 cluster_name={}
-with codecs.open("precision.json","r","utf-8") as f:
+
+#open the training files and read data
+with codecs.open("recall.json","r","utf-8") as f:
     lines = f.readlines() # read only the first tweet/line
 
-with codecs.open("precision_oriented.tsv","r","utf-8") as f:
+with codecs.open("recall_oriented.tsv","r","utf-8") as f:
     tsv=f.readlines()
 
 with open("cluster.txt","r") as f:
     clusters=f.readlines()
 
+#segregate into respective clusters
 language_id=[]
 language_value=[]
 cluster_id=[]
 for cluster in clusters:
-    print(cluster)
     str=cluster.replace("\n","")
     str=str.split("   ")
     cluster_name[str[1]]=str[0]
-    #cluster_id.append(str[1])
 
+#correlate tsv id's to json id's and extract tweet data
 for line in tsv:
     language=line.replace("\n","").split("\t")
     language_id.append(language[1])
@@ -37,13 +58,11 @@ for line in tsv:
         language_list.append(language[0])
         language_dict[language[0]]=[]
 
-
+#remove the [] around the tweets
 i=0
 del lines[-1]
 for line in lines:
-    #print(line)
     formatted=line.split(",",1)
-    #print(formatted)
     formatted[0]=formatted[0][2:-1]
     if len(formatted[1])>2:
         formatted[1]=formatted[1][:-2]
@@ -51,28 +70,20 @@ for line in lines:
     language_dict[language_full[formatted[0]]].append(formatted[1])
     i=i+1
 
-items=[]
-with codecs.open("train_precision_oriented.txt","w","utf-8") as f:
-    f_test=codecs.open("test_precision_oriented.txt","w","utf-8")
+#write data in files
+with codecs.open("train_recall_oriented.txt","w","utf-8") as f:
+    f_test=codecs.open("test_recall_oriented.txt","w","utf-8")
     for item in language_dict:
         value=language_dict[item]
         length=len(value)
-        train=int(0.8*length)
+        train=int(0.8*length)                                       #train set is 80%
         if train==0 and length>0:
             train=1
-        print(train)
-        print(length-train)
-        print(length)
-        print("-----------------------------------------------")
-        test=length-train
+        test=length-train                                           #test set is remaining 20%
         for i in range(0,train):
-            items.append(item)
-            text = re.sub(r"http\S+", "", value[i])
-            f.write(cluster_name[item]+"\t"+item+"\t"+text+"\n")
+            text=regex(value[i])
+            f.write(cluster_name[item]+"\t"+item+"\t"+text+"\n")    #write to train file
         for i in range(train,length):
-            items.append(item)
-            text = re.sub(r"http\S+", "", value[i])
-            f_test.write(cluster_name[item]+"\t"+item+"\t"+text+"\n")
+            text=regex(value[i])
+            f_test.write(cluster_name[item]+"\t"+item+"\t"+text+"\n")#write to test file
 f.close()
-
-#print(list(set(items)-set(cluster_id)))
